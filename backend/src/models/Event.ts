b509c9d4764b2cee2007dc16eaa2a0d26ebe1ade
caiocp <ccarvalho451@gmail.com>
2020-11-19 19:46:21 -0300

    add default values to columns

--- backend/src/models/Event.ts
+++ backend/src/models/Event.ts
@@ -23,7 +23,7 @@ export default class Event {
   @Column()
   value: string;
 
-  @Column()
+  @Column({default: 0})
   likes_amount: number;
 
   @Column()
@@ -35,6 +35,6 @@ export default class Event {
   @Column()
   longitude: string;
 
-  @Column()
+  @Column({default: true})
   active: boolean;
 }
